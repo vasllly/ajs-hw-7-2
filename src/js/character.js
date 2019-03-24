29f84af74894e7f6af50c4a57ad48647d0824a27
Ganja Vasiliy <vasllly219@gmail.com>
2019-03-24 22:25:05 +0300

    Done

---
+++ src/js/character.js
@@ -0,0 +1,33 @@
+/**
+ * Создает объект класса Character
+ *
+ * @param {string} name имя персонажа
+ * @param {string} type тип персонажа
+ *
+ * @throws {RangeError} Длина имени от 2 до 10 символов.
+ * @throws {TypeError} Такого типа персонажа не существует.
+ */
+export default function Character(name, type) {
+  if (name.length < 2 || name.length > 10) {
+    throw new RangeError('Длина имени от 2 до 10 символов');
+  }
+
+  const types = {
+    Bowman: '25/25',
+    Swordsman: '40/10',
+    Magician: '10/40',
+    Undead: '25/25',
+    Zombie: '40/10',
+    Daemon: '10/40',
+  };
+  if (!types[type]) {
+    throw new TypeError('Такого типа персонажа не существует');
+  }
+
+  this.name = name;
+  this.type = type;
+  this.health = 100;
+  this.level = 1;
+  this.attack = +types[type].split('/')[0];
+  this.defence = +types[type].split('/')[1];
+}
--- test/test.js
+++ test/test.js
@@ -1,5 +1,35 @@
-test('test', () => {
-  const received = 1 + 1;
-  const expected = 2;
-  expect(received).toBe(expected);
+import Character from '../src/js/character';
+
+test('test for Swordsman', () => {
+  const received = new Character('Мечник', 'Swordsman');
+  const expected = {
+    attack: 40,
+    defence: 10,
+    health: 100,
+    level: 1,
+    name: 'Мечник',
+    type: 'Swordsman',
+  };
+  expect(received).toEqual(expected);
+});
+
+test('test for name < 2', () => {
+  function newCharacter() {
+    Character('Л', 'Swordsman');
+  }
+  expect(newCharacter).toThrowError(new RangeError('Длина имени от 2 до 10 символов'));
+});
+
+test('test for name > 10', () => {
+  function newCharacter() {
+    Character('Дигидрогалаген', 'Swordsman');
+  }
+  expect(newCharacter).toThrowError(new RangeError('Длина имени от 2 до 10 символов'));
+});
+
+test('test for typeError', () => {
+  function newCharacter() {
+    Character('Лучник', 'Bowmen');
+  }
+  expect(newCharacter).toThrowError(new TypeError('Такого типа персонажа не существует'));
 });
